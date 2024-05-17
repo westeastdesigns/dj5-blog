976d7ef4eeb57d188374c535e88eda78082e1233
westeastdesigns <47990949+westeastdesigns@users.noreply.github.com>
2024-05-16 23:59:53 -0400

    implemented blog post pagination

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>/', 
-        views.post_detail, 
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
         name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,14 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    """post_list displays the list of posts
+    """post_list paginates and displays the list of all posts.
+    paginator assigned by Paginator class returns 3 posts from post_list per page.
+    page_number variable stores the page GET HTTP parameter, page 1 loads by default.
+    posts variable stores objects from the chosen Page object from the page() method.
 
     Args:
         request (object): required by all views
@@ -12,12 +16,19 @@ def post_list(request):
     Returns:
         url (list.html): renders the list of posts with the given template
     """
-    posts = Post.published.all()
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+    post_list = Post.published.all()
+    # Paginates with three posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not an integer, get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range, get last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -25,7 +36,11 @@ def post_detail(request, year, month, day, post):
 
     Args:
         request (object): required by all views
-        id (int): this view takes the id argument of a post to identify which post to show
+        view takes these arguments of a post to identify which post to show:
+        slug (string): unique slug identifying post
+        year (int): year post was published
+        month (int): month post was published
+        day (int): day post was published
 
     Raises:
         Http404: if no object is found
@@ -39,9 +54,6 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day)
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
+        publish__day=day,
     )
+    return render(request, "blog/post/detail.html", {"post": post})
